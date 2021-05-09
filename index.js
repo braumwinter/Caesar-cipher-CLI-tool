8c2b20d2bfd1c1e27d66b206db42942e5f293710
Kate <braumwinter@gmail.com>
2021-05-09 22:27:54 +0300

    feat: add main

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
- 
+const fs = require('fs');
+const {
+    getConsoleData
+} = require('./console_data');
+const {
+    transform
+} = require('./transformation');
+
+function main() {
+    const consoleData = getConsoleData();
+
+    const writeStream = fs.createWriteStream(consoleData.output);
+    const readStream = fs.createReadStream(consoleData.input);
+    readStream.on('data', function (data) {
+        const transformedData = transform(consoleData, data.toString());
+        writeStream.write(transformedData);
+    });
+}
+
+main();
